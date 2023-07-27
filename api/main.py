2565c9415d7aa1a70924b8537a8f613a35b6808d
Taras Bilobran <tbilobran@n-ix.com>
2023-07-27 20:22:59 +0300

    Implement listing locations for endpoint.

--- api/main.py
+++ api/main.py
@@ -3,6 +3,8 @@ from typing import List
 import pydantic
 from fastapi import FastAPI
 
+from app_context import default_app_context
+from repositories import ReportStatus
 
 api = FastAPI()
 
@@ -15,12 +17,22 @@ class ReportsResponse(pydantic.BaseModel):
     locations: List[str]
 
 
-@api.get("get_status/", response_model=HealthResponse)
+@api.get("/get_status", response_model=HealthResponse)
 async def check_health():
     return HealthResponse(status=True)
 
 
-@api.get("get_reports_locations/", response_model=ReportsResponse)
+@api.get("/get_reports_locations", response_model=ReportsResponse)
 async def get_reports_locations():
-    # TODO: Fetch data from DB.
-    return ReportsResponse(locations=[])
+    async with default_app_context() as context:
+        async with context.db_pool() as pool:
+            async with pool.acquire() as conn:
+                rows = await conn.fetch(
+                    """
+                    SELECT location
+                    FROM public.poison_report
+                    WHERE status = $1;
+                    """,
+                    ReportStatus.ACTIVE
+                )
+    return ReportsResponse(locations=[row["location"] for row in rows])
